92042d4565ede72f7442d77b4fefe466443ec48c
yuritsuprun <yuri.tsuprun@gmail.com>
2020-01-22 22:38:43 +0200

    changes to key_pair.tf

--- modules/compute/key_pair.tf
+++ modules/compute/key_pair.tf
@@ -10,5 +10,10 @@ resource "aws_key_pair" "server_key" {
 
 resource "local_file" "server_key" {
   sensitive_content = tls_private_key.server_key.private_key_pem
-  filename = "demo.pem"
+  filename = "project.pem"
+}
+
+resource "local_file" "server_key_public" {
+  sensitive_content = tls_private_key.server_key.public_key_pem
+  filename = "project.pub"
 }
