af6a5c576524e66551f861f125002ef4574378d1
pincher95 <yuri.tsuprun@gmail.com>
2020-01-27 15:03:33 +0200

    provision added

--- modules/jenkins/jenkins.tf
+++ modules/jenkins/jenkins.tf
@@ -42,19 +42,19 @@ resource "aws_instance" "jenkins_master" {
     bastion_user = "ubuntu"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt install docker.io -y",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo usermod -aG docker ubuntu",
-      "mkdir -p ${local.jenkins_home}",
-      "sudo chown -R 1000:1000 ${local.jenkins_home}",
-      "sleep 30",
-      "sudo docker run -d -p 8080:8080 -p 50000:50000 -v ${local.jenkins_home_mount} -v ${local.docker_sock_mount} --env ${local.java_opts} jenkins/jenkins"
-    ]
-  }
+//  provisioner "remote-exec" {
+//    inline = [
+//      "sudo apt-get update -y",
+//      "sudo apt install docker.io -y",
+//      "sudo systemctl start docker",
+//      "sudo systemctl enable docker",
+//      "sudo usermod -aG docker ubuntu",
+//      "mkdir -p ${local.jenkins_home}",
+//      "sudo chown -R 1000:1000 ${local.jenkins_home}",
+//      "sleep 30",
+//      "sudo docker run -d -p 8080:8080 -p 50000:50000 -v ${local.jenkins_home_mount} -v ${local.docker_sock_mount} --env ${local.java_opts} jenkins/jenkins"
+//    ]
+//  }
 
     provisioner "local-exec" {
       working_dir = "../ansible"
@@ -68,35 +68,35 @@ resource "aws_instance" "jenkins_master" {
   }
 }
 
-resource "aws_instance" "jenkins_agent" {
-  ami = "ami-00068cd7555f543d5"
-  instance_type = var.jenkis_ec2_type
-  subnet_id = var.private_subnet[1]
-  key_name = var.public_aws_key[0]
-  vpc_security_group_ids = [var.jenkis_sg, var.private_sg]
-
-  connection {
-    host = self.private_ip
-    user = "ec2-user"
-    private_key = file(var.private_key_path)
-
-    bastion_host = var.bastion_ip[0]
-    bastion_user = "ubuntu"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo yum install java-1.8.0 docker git -y",
-      "sudo alternatives --install /usr/bin/java java /usr/java/latest/bin/java 1",
-      "sudo alternatives --config java <<< '1'",
-      "sudo service docker start",
-      "sudo usermod -aG docker ec2-user"
-    ]
-  }
-
-
-  tags = {
-    Name = "Jenkins Agent"
-  }
-}
+//resource "aws_instance" "jenkins_agent" {
+//  ami = "ami-00068cd7555f543d5"
+//  instance_type = var.jenkis_ec2_type
+//  subnet_id = var.private_subnet[1]
+//  key_name = var.public_aws_key[0]
+//  vpc_security_group_ids = [var.jenkis_sg, var.private_sg]
+//
+//  connection {
+//    host = self.private_ip
+//    user = "ec2-user"
+//    private_key = file(var.private_key_path)
+//
+//    bastion_host = var.bastion_ip[0]
+//    bastion_user = "ubuntu"
+//  }
+//
+//  provisioner "remote-exec" {
+//    inline = [
+//      "sudo yum update -y",
+//      "sudo yum install java-1.8.0 docker git -y",
+//      "sudo alternatives --install /usr/bin/java java /usr/java/latest/bin/java 1",
+//      "sudo alternatives --config java <<< '1'",
+//      "sudo service docker start",
+//      "sudo usermod -aG docker ec2-user"
+//    ]
+//  }
+//
+//
+//  tags = {
+//    Name = "Jenkins Agent"
+//  }
+//}
