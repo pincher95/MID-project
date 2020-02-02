7f5731a621c5368072f5ad42a509788232c12a6b
yuritsuprun <yuri.tsuprun@gmail.com>
2020-02-02 16:21:49 +0200

    Jenkinsfile bug fix

--- modules/jenkins/jenkins_agent.tf
+++ modules/jenkins/jenkins_agent.tf
@@ -1,27 +1,28 @@
 # Create an IAM role for eks kubectl
-resource "aws_iam_role" "eks-kubectl" {
-  name               = "opsschool-eks-kubectl"
-  assume_role_policy = file("./policy/assume-eks-role.json")
+resource "aws_iam_role" "eks-full" {
+  description = "Accessing all of account EKS cluster API endpoints"
+  name               = "opsschool-eks-full"
+  assume_role_policy = file("./policy/assume-policy.json")
 }
 
 # Create the policy
-resource "aws_iam_policy" "eks-kubectl" {
-  name        = "opsschool-eks-kubectl"
-  description = "Allows unubtu node to run kubectl."
-  policy      = file("./policy/describe-eks-cluster.json")
+resource "aws_iam_policy" "eks-full" {
+  description = "EKS Full access policy"
+  name        = "opsschool-eks-full"
+  policy      = file("./policy/eks-full.json")
 }
 
 # Attach the policy
-resource "aws_iam_policy_attachment" "eks-kubectl" {
-  name       = "opsschool-eks-kubectl"
-  roles      = [aws_iam_role.eks-kubectl.name]
-  policy_arn = aws_iam_policy.eks-kubectl.arn
+resource "aws_iam_policy_attachment" "eks-full" {
+  name       = "opsschool-eks-full"
+  roles      = [aws_iam_role.eks-full.name]
+  policy_arn = aws_iam_policy.eks-full.arn
 }
 
 # Create the instance profile
 resource "aws_iam_instance_profile" "eks-kubectl" {
-  name  = "opsschool-eks-kubectl"
-  role = aws_iam_role.eks-kubectl.name
+  name  = "opsschool-eks-full"
+  role = aws_iam_role.eks-full.name
 }
 
 
