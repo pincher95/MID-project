3faedc83e31c3508a651d89dc52771ebcb031937
pincher95 <yuri.tsuprun@gmail.com>
2020-02-24 14:01:20 +0200

    pipeline bug fix

--- modules/key-pair/key_pair.tf
+++ modules/key-pair/key_pair.tf
@@ -14,19 +14,19 @@ resource "local_file" "server_key_private" {
   count = length(var.key_pair)
   sensitive_content = tls_private_key.server_key[count.index].private_key_pem
   filename = "../${path.root}/keys/${element(var.key_pair, count.index)}.pem"
-  file_permission = "0400"
+  file_permission = "0600"
 }
 
 resource "local_file" "server_key_public" {
   count = length(var.key_pair)
   sensitive_content = tls_private_key.server_key[count.index].public_key_pem
   filename = "../${path.root}/keys/${element(var.key_pair, count.index)}.pub"
-  file_permission = "0400"
+  file_permission = "0600"
 }
 
 resource "local_file" "authorized_keys" {
   count = length(var.key_pair)
   sensitive_content = tls_private_key.server_key[count.index].public_key_openssh
   filename = "../${path.root}/keys/authorized_keys"
-  file_permission = "0400"
+  file_permission = "0600"
 }
