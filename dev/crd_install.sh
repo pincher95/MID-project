798f0e966b5829823095db4ed369895fcf276502
yuritsuprun <yuri.tsuprun@gmail.com>
2020-04-12 12:24:47 +0300

    added helm charts for elastic stack, prometheus, grafana and jenkins

--- dev/crd_install.sh
+++ dev/crd_install.sh
@@ -6,4 +6,3 @@ echo "Deploying prometheus-operator CRD's"
 for val in ${DataList[*]}; do
   kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/release-0.37/example/prometheus-operator-crd/monitoring.coreos.com_${val}.yaml --validate=false
 done
-
